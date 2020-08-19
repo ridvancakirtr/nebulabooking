9ec2044627fe52160cfc39db83ac3f5182e82738
Nazmi Sefa Kelleci <nskelleci@gmail.com>
2020-08-19 23:17:52 +0300

    gfd

--- backend/middlewares/authorization/auth.js
+++ backend/middlewares/authorization/auth.js
@@ -3,7 +3,7 @@ const jwt = require ("jsonwebtoken")
 const {isTokenIncluded,getAccessTokenFromHeader} = require("../../Helpers/authorization/tokenHelpers")
 
 const getAccessToRoute = (req,res,next)=>{
-    // TOKEN 
+    // TOKEN fdfddsfs
     const {JWT_SECRET_KEY} = process.env;
     if(!isTokenIncluded(req)){
         return next(new CustomError("You are not authorized to access",401))
