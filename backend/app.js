d7c637a55de8fe8f46e16d8f1eee8a286df5a7c5
Nazmi Sefa Kelleci <nskelleci@gmail.com>
2020-08-19 23:13:33 +0300

    new message

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const dotenv = require('dotenv')
+const express = require('express');
+const connectDatabase = require('./Helpers/Database/connect-database');
+const routers = require("./routes/main-router");
+const customErrorHandler = require("./middlewares/errors/custom-error-handler");
+
+//Enviroment variables
+dotenv.config({
+    path : "./config/env/config.env"
+})
+
+// MongoDB Connection
+connectDatabase();
+
+const PORT = process.env.PORT;
+
+
+const app = express();
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+//ROUTES MIDDLEWARE
+app.use("/api/v1",routers)
+//ROUTES END
+
+//Error Handler
+app.use(customErrorHandler)
+
+
+app.get('/',(req,res)=>{
+    res.send({
+        message:'hello world'
+    })
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`App Started on : ${PORT} : ${process.env.NODE_ENV}`)
+});
