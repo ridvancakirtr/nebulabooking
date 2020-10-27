b643e8e0f12a08aa633d11abd69af1b79f36cd77
Nazmi Sefa Kelleci <nskelleci@gmail.com>
2020-10-27 12:16:10 +0300

    update agency route added

--- backend/routes/agency-type.js
+++ backend/routes/agency-type.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {addAgencyType, getAll} = require("../controllers/agencyTypeController")
+const {addAgencyType, getAll, updateAgencyType} = require("../controllers/agencyTypeController")
 
 router.post("/add", addAgencyType)
 router.get("/", getAll)
+router.put("/updateAgencyType/:id", updateAgencyType)
 
 
 module.exports = router
