8889fd3f50aefd74824de05c75395bf161ffdaa8
Nazmi Sefa Kelleci <nskelleci@gmail.com>
2020-11-23 18:06:43 +0300

    rewwres

--- backend/app.js
+++ backend/app.js
@@ -28,10 +28,10 @@ app.use(bodyParser.json())
 
 //CORS 
 
-var whitelist = ['http://localhost:8080', 'https://nebula-booking-vue.herokuapp.com', '*']
+var whitelist = ['http://localhost:8080', 'https://nebula-booking-vue.herokuapp.com', '5.182.247.156']
 
 app.use(cors({
-    origin : whitelist[2],
+    origin : whitelist[0],
     credentials : true,
 }))
 
